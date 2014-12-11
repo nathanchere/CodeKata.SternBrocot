08f447b3fa1aa180c52809430abcc5d84a9155c5
Nathan Chere <git@nathanchere.com.au>
2014-12-11 23:14:20 +0100

    Tests

--- SternBrocotSequenceGenerator.Core/Sequence.cs
+++ SternBrocotSequenceGenerator.Core/Sequence.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace SternBrocotSequenceGenerator
 {
@@ -13,13 +15,24 @@ namespace SternBrocotSequenceGenerator
         {
             var x = Fibonacci();
 
-            int a = 
-            
+            int a = x.First();
+            throw new Exception();
 
         }
 
         public static IEnumerable<int> Fibonacci()
         {
+            int a = 1;
+            int b = 1;
+            int current;
+
+            while (true)
+            {
+                current = a + b;
+                yield return current;
+                a = b;
+                b = current;
+            }                        
         }
     }
 }
--- SternBrocotSequenceGenerator.Tests/Class1.cs
+++ SternBrocotSequenceGenerator.Tests/Class1.cs
@@ -7,42 +7,41 @@ using Xunit.Extensions;
 namespace SternBrocotSequenceGenerator
 {
     public class FibonacciSequenceTests
-    {        
-        [Theory]        
-        [InlineData(1,1)]
-        [InlineData(2,1)]
-        [InlineData(3,2)]
-        [InlineData(4,3)]
-        [InlineData(5,5)]
-        [InlineData(6,8)]
-        [InlineData(7,13)]
-        [InlineData(8,21)]
-        [InlineData(9,34)]
-        [InlineData(10,55)]
+    {
+        [Theory]
+        [InlineData(1, 1)]
+        [InlineData(2, 1)]
+        [InlineData(3, 2)]
+        [InlineData(4, 3)]
+        [InlineData(5, 5)]
+        [InlineData(6, 8)]
+        [InlineData(7, 13)]
+        [InlineData(8, 21)]
+        [InlineData(9, 34)]
+        [InlineData(10, 55)]
         public void Generates_expected_sequence_values(int iteration, int expected)
-        {            
+        {
             var result = Sequence.Fibonacci().Skip(iteration).First();
 
             Assert.Equal(expected, result);
-        }
         }
     }
 
     public class SternBrocotSeedTests
     {
-        [Theory]        
-        [InlineData(1,1)]
-        [InlineData(2,1)]
-        [InlineData(3,2)]
-        [InlineData(4,1)]
-        [InlineData(5,3)]
-        [InlineData(6,2)]
-        [InlineData(7,3)]
-        [InlineData(8,1)]
-        [InlineData(9,4)]
-        [InlineData(10,3)]
+        [Theory]
+        [InlineData(1, 1)]
+        [InlineData(2, 1)]
+        [InlineData(3, 2)]
+        [InlineData(4, 1)]
+        [InlineData(5, 3)]
+        [InlineData(6, 2)]
+        [InlineData(7, 3)]
+        [InlineData(8, 1)]
+        [InlineData(9, 4)]
+        [InlineData(10, 3)]
         public void Generates_expected_sequence_values(int iteration, int expected)
-        {            
+        {
             var result = Sequence.SternBrocotSeeds().Skip(iteration).First();
 
             Assert.Equal(expected, result);
@@ -52,19 +51,19 @@ namespace SternBrocotSequenceGenerator
     public class SternBrocotTests
     {
         [Theory]
-        [InlineData(1,1,1)]
-        [InlineData(2,1,2)]
-        [InlineData(3,2,1)]
-        [InlineData(4,1,3)]
-        [InlineData(5,3,2)]
-        [InlineData(6,2,3)]
-        [InlineData(7,3,1)]
-        [InlineData(8,1,4)]
-        [InlineData(9,4,3)]
-        [InlineData(10,3,5)]
+        [InlineData(1, 1, 1)]
+        [InlineData(2, 1, 2)]
+        [InlineData(3, 2, 1)]
+        [InlineData(4, 1, 3)]
+        [InlineData(5, 3, 2)]
+        [InlineData(6, 2, 3)]
+        [InlineData(7, 3, 1)]
+        [InlineData(8, 1, 4)]
+        [InlineData(9, 4, 3)]
+        [InlineData(10, 3, 5)]
         public void Generates_expected_sequence_values(int iteration, int expectedDividend, int expectedDivisor)
         {
-            var expected = new Fraction(expectedDividend,expectedDivisor);
+            var expected = new Fraction(expectedDividend, expectedDivisor);
             var result = Sequence.SternBrocot().Skip(iteration).First();
 
             Assert.Equal(expected, result);
