a2530428aca9f598e833d0bd723906efc2f4dd34
Nathan Chere <git@nathanchere.com.au>
2014-12-11 19:06:15 +0100

    Last fraction tests

--- SternBrocotSequenceGenerator.Tests/Class1.cs
+++ SternBrocotSequenceGenerator.Tests/Class1.cs
@@ -39,5 +39,20 @@ namespace SternBrocotSequenceGenerator.Tests
             Assert.Equal(expectedTop, result.Dividend);
             Assert.Equal(expectedBottom, result.Divisor);
         }
+
+        [Theory]
+        [InlineData(1, 1, 1, 1)]
+        [InlineData(0, 1, 0, 1)]
+        [InlineData(1, 2, 1, 2)]
+        [InlineData(2,1, 2, 1)]
+        [InlineData(-1,1, -1, 1)]
+        [InlineData(1, -1, 1, -1)]
+        [InlineData(1, -1, 1, -1)]
+        public void parses_integers_correctly(int dividend, int divisor, int expectedTop, int expectedBottom)
+        {
+            var result = new Fraction(dividend, divisor);
+            Assert.Equal(expectedTop, result.Dividend);
+            Assert.Equal(expectedBottom, result.Divisor);
+        }
     }
 }
