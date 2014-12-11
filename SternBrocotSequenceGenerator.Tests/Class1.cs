9ced5215180c12fd064296ea1b9196e5e66297a7
Nathan Chere <git@nathanchere.com.au>
2014-12-11 19:03:58 +0100

    Fraction tests

--- SternBrocotSequenceGenerator.Tests/Class1.cs
+++ SternBrocotSequenceGenerator.Tests/Class1.cs
@@ -2,6 +2,8 @@
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
+using Xunit.Extensions;
 
 namespace SternBrocotSequenceGenerator.Tests
 {
@@ -17,7 +19,25 @@ namespace SternBrocotSequenceGenerator.Tests
     {
         public void Generates_expected_sequence_values()
         {
-            var expected = new Fraction { };
+            //var expected = new Fraction { };
+        }
+    }
+
+    public class FractionTests
+    {
+        [Theory]
+        [InlineData("1/1", 1, 1)]
+        [InlineData("0/1", 0, 1)]
+        [InlineData("1/2", 1, 2)]
+        [InlineData("2/1", 2, 1)]
+        [InlineData("-1/1", -1, 1)]
+        [InlineData("1/-1", 1, -1)]
+        [InlineData("-1/-1", -1, -1)]
+        public void parses_strings_correctly(string input, int expectedTop, int expectedBottom)
+        {
+            var result = new Fraction(input);
+            Assert.Equal(expectedTop, result.Dividend);
+            Assert.Equal(expectedBottom, result.Divisor);
         }
     }
 }
