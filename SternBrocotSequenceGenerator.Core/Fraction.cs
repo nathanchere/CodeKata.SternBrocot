89e6f66a4ae8f5bff6075fca533b16fecb76aadb
Nathan Chere <git@nathanchere.com.au>
2014-12-11 18:50:58 +0100

    FrCTION stuff

--- SternBrocotSequenceGenerator.Core/Fraction.cs
+++ SternBrocotSequenceGenerator.Core/Fraction.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace SternBrocotSequenceGenerator
 {
@@ -7,11 +8,32 @@ namespace SternBrocotSequenceGenerator
         private int _dividend;
         private int _divisor;
 
-        public Fraction(int dividend, int divisor)
+        private void Initialise(int dividend, int divisor)
         {
-            if(divisor == 0) throw new ArgumentOutOfRangeException("Divisor may not be zero");
+            if (divisor == 0) throw new ArgumentOutOfRangeException("Divisor may not be zero");
             _dividend = dividend;
             _divisor = divisor;
+        }
+
+        public Fraction(int dividend, int divisor)
+        {
+            Initialise(dividend, divisor);
+        }
+
+        public Fraction(string text)
+        {
+            if(string.IsNullOrEmpty(text)
+                || !text.Contains("/")
+                || text.Split('/').Count() != 2)
+                throw new ArgumentException("Not a valid fraction");
+
+            var result = text.Split('/');
+            int dividend = 0, divisor = 0;
+            if (!Int32.TryParse(result[0], out dividend)
+                || !Int32.TryParse(result[1], out divisor))
+                throw new ArgumentException("Not a valid fraction");
+
+            Initialise(dividend, divisor);
         }
 
         public override string ToString()
