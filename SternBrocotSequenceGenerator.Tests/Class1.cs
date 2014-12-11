0bc598e49722bc7840f04766abfa47ace373d66d
Nathan Chere <git@nathanchere.com.au>
2014-12-11 18:36:39 +0100

    Fix namespaces

--- SternBrocotSequenceGenerator.Tests/Class1.cs
+++ SternBrocotSequenceGenerator.Tests/Class1.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,24 +18,6 @@ namespace SternBrocotSequenceGenerator.Tests
         public void Generates_expected_sequence_values()
         {
             var expected = new Fraction { };
-        }
-    }
-
-    public class Fraction
-    {
-        private int _dividend;
-        private int _divisor;
-
-        public Fraction(int dividend, int divisor)
-        {
-            if(divisor == 0) throw new ArgumentOutOfRangeException("Divisor may not be zero");
-            _dividend = dividend;
-            _divisor = divisor;
-        }
-
-        public override string ToString()
-        {
-            return string.Format("{0}/{1}", _dividend, _divisor);
         }
     }
 }
