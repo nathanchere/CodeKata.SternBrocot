6ea514dcc0eea162cd1b6ebef580d50dae026eea
Nathan Chere <git@nathanchere.com.au>
2014-12-11 18:05:38 +0100

    Fraction to core

---
+++ SternBrocotSequenceGenerator.Core/Fraction.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SternBrocotSequenceGenerator
+{
+    public class Fraction
+    {
+        private int _dividend;
+        private int _divisor;
+
+        public Fraction(int dividend, int divisor)
+        {
+            if(divisor == 0) throw new ArgumentOutOfRangeException("Divisor may not be zero");
+            _dividend = dividend;
+            _divisor = divisor;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0}/{1}", _dividend, _divisor);
+        }
+    }
+}
